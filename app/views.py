53a13cb1e7f9ad7a597b5e7f5ad98edee420d3f2
Nelson Bassey <nellybaz10@gmail.com>
2022-04-08 21:21:01 +0200

    adds sample html templating

--- app/views.py
+++ app/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 
 def index(request):
-    context = {'data': 'data'}
+    context = {'name': 'There', 'place':'Alice in wonderland'}
     return render(request, 'app/index.html', context)
