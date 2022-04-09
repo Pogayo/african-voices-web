61757f2dd08f8e93d69c167e57a2b4adb0790a8e
Nelson Bassey <nellybaz10@gmail.com>
2022-04-09 07:18:03 +0200

    adds sample bash execution

--- app/views.py
+++ app/views.py
@@ -1,6 +1,22 @@
+#!/usr/bin/python
+
 from django.shortcuts import render
 from .models import Language
+from subprocess import call
 
 def index(request):
     context = {'name': 'There', 'place':'Alice in wonderland', 'languages': Language.objects.all() }
+
+    try:
+        execute_transcription()
+    except BaseException as e:
+        print(e)
+
     return render(request, 'app/index.html', context)
+
+
+def execute_transcription():
+    print("Executing bash command")
+    script = "/Users/nelsonbassey/Development/others/african_voices/transcribe.sh"
+    res = call(script, shell=True)
+    print(res)
---
+++ transcribe.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo " ***** Yes I got your command, transcribing luo text now *****"
+ls
+
+echo " ***** I am done *****"
+
