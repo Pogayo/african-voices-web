57a86452094739c85e55d2f94bf27835cd81a0e6
Perez Ogayo <perezogayo@gmail.com>
2022-04-09 02:23:39 -0400

    structured homepage

--- app/views.py
+++ app/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from .models import Language
 
 def index(request):
-    context = {'name': 'There', 'place':'Alice in wonderland', 'languages': Language.objects.all() }
+    context = {'name': 'There', 'place':'Alice in wonderland', 'languages': Language.objects.all(),
+               'num_synthesizers':  20, "num_countries":  20, "num_langs":  11, "num_speakers":  14, "num_hours":  600 }
     return render(request, 'app/index.html', context)
