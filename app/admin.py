cba1ee6f0e8fdbd7d9aa860fb0667e2477aa0098
Perez Ogayo <perezogayo@gmail.com>
2022-04-10 15:18:41 -0400

    displayed datasets for a language

--- app/admin.py
+++ app/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import *
 
 
-@admin.register(Language, Country, LanguageCountry, LanguageSample, Dataset, Synthesizer, SynthesizerSample)
+@admin.register(Language, Country,  LanguageSample, Dataset, Synthesizer, SynthesizerSample)
 class UniversalAdmin(admin.ModelAdmin):
     def get_list_display(self, request):
         return [field.name for field in self.model._meta.concrete_fields]
--- app/models/language.py
+++ app/models/language.py
@@ -1,5 +1,7 @@
-from django.db import models
 from uuid import uuid4
+
+from django.db import models
+
 from .country import Country
 
 
@@ -12,15 +14,22 @@ class Language(models.Model):
     lang_non_native_speakers = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    countries = models.ManyToManyField(Country)
 
     def __str__(self) -> str:
         return self.lang_name
 
+    @property
+    def total_speakers(self) -> int:
+        return self.lang_native_speakers + self.lang_non_native_speakers
 
-class LanguageCountry(models.Model):
-    uuid = models.CharField(primary_key=True, unique=True, default=uuid4, max_length=100)
-    language = models.ManyToManyField(Language)
-    country = models.ManyToManyField(Country)
+    @property
+    def all_countries(self) -> str:
+        return ", ".join([country.name.capitalize() for country in self.countries.all()])
+
+    @property
+    def datasets(self):
+        return self.dataset_set.all()
 
 
 class LanguageSample(models.Model):
